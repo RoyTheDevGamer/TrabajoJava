6488372b2defdd0181d35b329ea94f2ccaef6b2b
roycr <roycruz7654@gmail.com>
2024-09-02 16:26:56 -0500

    Supuesta Calculadora

--- CalculatorFX/src/main/java/pe/edu/upeu/calfx/CalculatorFxApplication.java
+++ CalculatorFX/src/main/java/pe/edu/upeu/calfx/CalculatorFxApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorFxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorFxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorFxApplication.class, args);
+    }
 
 }
---
+++ CalculatorFX/src/main/java/pe/edu/upeu/calfx/controlador/MainController.java
@@ -0,0 +1,56 @@
+package pe.edu.upeu.calfx.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button button0;
+    @FXML
+    private Button buttonSuma;
+    @FXML
+    private Button buttonResta;
+    @FXML
+    private Button buttonMultiplicar;
+    @FXML
+    private Button buttonDividir;
+    @FXML
+    private Button buttonPorcentage;
+    @FXML
+    private Button buttonLimpiar;
+    @FXML
+    private Button buttonBorrar;
+    @FXML
+    private Button buttonIgual;
+    @FXML
+    private Button buttonAns;
+    @FXML
+    private Button buttonComa;
+    public void limpiar(){
+
+    }
+    public void eliminar(){
+
+    }public void operacion(){
+
+    }
+
+}
