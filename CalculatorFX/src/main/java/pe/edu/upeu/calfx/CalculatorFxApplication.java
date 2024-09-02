9405e96eb4629000581fbb7e4ece4e90dd477346
roycr <roycruz7654@gmail.com>
2024-09-02 13:36:49 -0500

    App de escritorio Listaa v1

---
+++ CalculatorFX/src/main/java/pe/edu/upeu/calfx/CalculatorFxApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.calfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorFxApplication.class, args);
+	}
+
+}
---
+++ CalculatorFX/src/test/java/pe/edu/upeu/calfx/CalculatorFxApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.calfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
